a959a27f5e24ad5e842a3accad0e58b0a23e4409
Fabian Olsson <fabtasticdesign@live.se>
2020-01-14 09:43:59 +0100

    moved dependency to plugin folder

--- includes/class-FB_Dependencies.php
+++ includes/class-FB_Dependencies.php
@@ -3,7 +3,7 @@
 class FB_Dependencies {
 
     public static function load_dependencies(){
-        require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/facebook/graph-sdk/src/Facebook/autoload.php';
+        require_once plugin_dir_path( __DIR__ ) . "facebook/graph-sdk/src/Facebook/autoload.php";
         require_once "class-FB_Keys.php";
         require_once "class-FB_Url_Settup.php";
         require_once "class-FB_Loop.php";
